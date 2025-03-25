b2cee70608bc6c7442b4d051e9faad32c73e6e12
Ryan Milrad <ryanmilrad@Ryans-MacBook-Pro-2.local>
2025-03-24 20:34:41 -0600

    main

--- src/data/digitalProductItems.ts
+++ src/data/digitalProductItems.ts
@@ -11,103 +11,103 @@ import { PortfolioItem } from "@/types/portfolio";
 export const digitalProductItems: PortfolioItem[] = [
   {
     id: 1,
-    image: "/imgs/digital_products/1.png",
+    image: "./imgs/digital_products/1.png",
     titleKey: "digitalProduct1",
     descriptionKey: "empoweredParent"
   },
   {
     id: 2,
-    image: "/imgs/digital_products/2.png",
+    image: "./imgs/digital_products/2.png",
     titleKey: "digitalProduct2",
     descriptionKey: "moduleIntro"
   },
   {
     id: 3,
-    image: "/imgs/digital_products/3.png",
+    image: "./imgs/digital_products/3.png",
     titleKey: "digitalProduct3",
     descriptionKey: "selfCare"
   },
   {
     id: 4,
-    image: "/imgs/digital_products/4.png",
+    image: "./imgs/digital_products/4.png",
     titleKey: "digitalProduct4",
     descriptionKey: "takeCareOfYourself"
   },
   {
     id: 5,
-    image: "/imgs/digital_products/5.png",
+    image: "./imgs/digital_products/5.png",
     titleKey: "digitalProduct5",
     descriptionKey: "sneakInYouTime"
   },
   {
     id: 6,
-    image: "/imgs/digital_products/6.png",
+    image: "./imgs/digital_products/6.png",
     titleKey: "digitalProduct6",
     descriptionKey: "connectBeforeCorrect"
   },
   {
     id: 7,
-    image: "/imgs/digital_products/7.png",
+    image: "./imgs/digital_products/7.png",
     titleKey: "digitalProduct7",
     descriptionKey: "connectingReal"
   },
   {
     id: 8,
-    image: "/imgs/digital_products/8.png",
+    image: "./imgs/digital_products/8.png",
     titleKey: "digitalProduct8",
     descriptionKey: "parentingTool"
   },
   {
     id: 9,
-    image: "/imgs/digital_products/9.png",
+    image: "./imgs/digital_products/9.png",
     titleKey: "digitalProduct9",
     descriptionKey: "stressResets"
   },
   {
     id: 10,
-    image: "/imgs/digital_products/10.png",
+    image: "./imgs/digital_products/10.png",
     titleKey: "digitalProduct10",
     descriptionKey: "aboutMe"
   },
   {
     id: 11,
-    image: "/imgs/digital_products/11.png",
+    image: "./imgs/digital_products/11.png",
     titleKey: "digitalProduct11",
     descriptionKey: "ebookUsage"
   },
   {
     id: 12,
-    image: "/imgs/digital_products/12.png",
+    image: "./imgs/digital_products/12.png",
     titleKey: "digitalProduct12",
     descriptionKey: "diaphragmaticBreathing"
   },
   {
     id: 13,
-    image: "/imgs/digital_products/13.png",
+    image: "./imgs/digital_products/13.png",
     titleKey: "digitalProduct13",
     descriptionKey: "twoStepBreathing"
   },
   {
     id: 14,
-    image: "/imgs/digital_products/14.png",
+    image: "./imgs/digital_products/14.png",
     titleKey: "digitalProduct14",
     descriptionKey: "journalExercise"
   },
   {
     id: 15,
-    image: "/imgs/digital_products/15.png",
+    image: "./imgs/digital_products/15.png",
     titleKey: "digitalProduct15",
     descriptionKey: "progressiveMuscleRelaxation"
   },
   {
     id: 16,
-    image: "/imgs/digital_products/16.png",
+    image: "./imgs/digital_products/16.png",
     titleKey: "digitalProduct16",
     descriptionKey: "pmrProcess"
   },
   {
     id: 17,
-    image: "/imgs/digital_products/17.png",
+    image: "./imgs/digital_products/17.png",
     titleKey: "digitalProduct17",
     descriptionKey: "presentMomentAwareness"
   }
